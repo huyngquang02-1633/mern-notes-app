38aaa2260523e93157504aff16088a3ee880ac92
huyngquang02-1633 <huy12042002yb@gmail.com>
2024-02-18 22:00:14 +0700

    day1

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config()
+
+const authRouter = require('./routes/auth')
+
+const connectDB = async () => {
+  try {
+    await mongoose
+      .connect(
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mearn-notes-app.9v06i5d.mongodb.net/?retryWrites=true&w=majority`
+      )
+      .then(() => {
+        console.log("Successfully connected to atlas");
+      });
+  } catch (error) {
+    console.log(`Something went wrong: ${error.message}`);
+  }
+};
+
+connectDB();
+
+const app = express();
+app.use(express.json())
+
+app.use("/api/auth", authRouter);
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
