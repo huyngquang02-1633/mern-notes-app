cb5714dffa65388d738ffe8b437443c2b8a478ee
huyngquang02-1633 <huy12042002yb@gmail.com>
2024-03-29 19:29:56 +0700

    jwt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -47,40 +47,61 @@ router.post("/register", async (req, res) => {
 //route POST api/auth/login
 //public access
 router.post("/login", async (req, res) => {
-    const { username, password } = req.body;
-  
-    if (!username || !password)
+  const { username, password } = req.body;
+
+  if (!username || !password)
+    return res
+      .status(400)
+      .json({ success: false, message: "Missing username and/or password" });
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
       return res
         .status(400)
-        .json({ success: false, message: "Missing username and/or password" });
-  
-    try {
-      const user = await User.findOne({ username });
-  
-      if (!user){
-        return res.status(400).json({ success: false, message: "Incorrect username or password!" });
-      }
-      //vetify password trong db cua user va password nguoi dung nhap
-      const passwordValid = await argon2.verify(user.password, password);
-      if(!passwordValid){
-        return res.status(400).json({ success: false, message: "Incorrect username or password!" });
-      }
-
-      //return token
-      const accessToken = jwt.sign(
-        { userId: user._id },
-        process.env.ACCESS_TOKEN_SECRET
-      );
-  
-      res.json({
-        success: true,
-        message: "Logged successfully",
-        accessToken,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ success: false, message: "Internal server error" });
+        .json({ success: false, message: "Incorrect username or password!" });
     }
-  });
+    //vetify password trong db cua user va password nguoi dung nhap
+    const passwordValid = await argon2.verify(user.password, password);
+    if (!passwordValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect username or password!" });
+    }
+
+    //return token
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    res.json({
+      success: true,
+      message: "Logged successfully",
+      accessToken,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+// @route GET api/auth
+// Check user is logged in
+// @access Public
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select("-password");
+    if (!user)
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+    res.json({ success: true, user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
 
 module.exports = router;
